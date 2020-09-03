aea5205001c073237dabe2b8ed268310aa22accf
whylucas <lucas.lago@live.com>
2020-09-02 23:05:33 -0300

    Criação do repositório no git

---
+++ LavCdS.GestaoCliente.Domain/Interface/ICliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LavCdS.GestaoCliente.Domain.Interface
+{
+    public interface ICliente
+    {
+        string Bairro { get; set; }
+        string Cep { get; set; }
+        string Cidade { get; set; }
+        string Complemento { get; set; }
+        DateTime Data_inclusao { get; set; }
+        int Documento { get; set; }
+        string Email { get; set; }
+        string Estado { get; set; }
+        int Id_cliente { get; set; }
+        string Logradouro { get; set; }
+        string Num_logradouro { get; set; }
+        string Pf_pj { get; set; }
+        string Razao_social { get; set; }
+        int Telefone1 { get; set; }
+        int Telefone2 { get; set; }
+    }
+}
---
+++ LavCdS.GestaoCliente.Infrastructure/Repositories/PostgreSQL/ClienteRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LavCdS.GestaoCliente.Domain.Interface;
+using LavCdS.GestaoCliente.Domain.Model;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+
+namespace LavCdS.GestaoCliente.Infrastructure.Repositories.PostgreSQL
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        IConfiguration configuration;
+        string connectionString;
+
+        public ClienteRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.connectionString = this.configuration.GetConnectionString("PostgreSQL");
+        }
+
+        public Cliente getClientePorId(int id_cliente)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=postgres;Database=teste;");
+            Cliente cliente = new Cliente();
+            return cliente;
+        }
+
+        public List<ICliente> getClientePorNome(string nome)
+        {
+            List<ICliente> clientes = new List<ICliente>();
+            return clientes;
+        }
+
+        public bool gravaCliente(ICliente cliente)
+        {
+            bool result = true;
+            return result;
+        }
+
+        public bool atualizaCliente(ICliente cliente)
+        {
+            bool result = true;
+            return result;
+        }
+
+
+    }
+}
